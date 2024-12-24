3a7a712d0e87667ed313ca8bc8e51c8060f2cb9f
acozy03 <acozy03@gmail.com>
2024-12-23 19:08:46 -0500

    dark and light mode

--- frontend/src/components/app-bar.tsx
+++ frontend/src/components/app-bar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
